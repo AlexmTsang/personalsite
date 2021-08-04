0315cdf5e244481c654049425e707a32af113b58
amtsang <amtsang@umich.edu>
2021-08-03 23:08:37 -0700

    Updates

---
+++ precache-manifest.5c0c8aefa9defe93227fd9114d3f13cf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5d37ffd75e81dd566f6dfa5552ce7d",
+    "url": "/personalsite/index.html"
+  },
+  {
+    "revision": "8176bec5328c27e72170",
+    "url": "/personalsite/static/css/main.714d396f.chunk.css"
+  },
+  {
+    "revision": "5decfde004ac8987d14d",
+    "url": "/personalsite/static/js/2.b97fd18b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personalsite/static/js/2.b97fd18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8176bec5328c27e72170",
+    "url": "/personalsite/static/js/main.777e6168.chunk.js"
+  },
+  {
+    "revision": "baccecc6ccfbfe998cce",
+    "url": "/personalsite/static/js/runtime-main.5a5b3059.js"
+  },
+  {
+    "revision": "dc5ae86c06d1c74a50da2c83ad99ec24",
+    "url": "/personalsite/static/media/Background.dc5ae86c.png"
+  },
+  {
+    "revision": "ec1a0dca35f34b26c2d51307f26dfc91",
+    "url": "/personalsite/static/media/Resume.ec1a0dca.pdf"
+  },
+  {
+    "revision": "80ac687a950a4855fc144126566d39e3",
+    "url": "/personalsite/static/media/aws.80ac687a.png"
+  },
+  {
+    "revision": "0b849c72f38362fe12072a4916660013",
+    "url": "/personalsite/static/media/cpp.0b849c72.png"
+  },
+  {
+    "revision": "71837401e9bf10020708e4716d4585fd",
+    "url": "/personalsite/static/media/css.71837401.png"
+  },
+  {
+    "revision": "8a9c29a90ac7cf42e477fe6c7ad5d009",
+    "url": "/personalsite/static/media/firebase.8a9c29a9.png"
+  },
+  {
+    "revision": "4bf78b295ba5cc2455d544780261ec4d",
+    "url": "/personalsite/static/media/git.4bf78b29.png"
+  },
+  {
+    "revision": "4d2fdd69a1412fe848034395d09941b1",
+    "url": "/personalsite/static/media/heroku.4d2fdd69.png"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/personalsite/static/media/java.652fdb65.png"
+  },
+  {
+    "revision": "d3304b833b96ca9b6b4d4f12d07d7176",
+    "url": "/personalsite/static/media/javascript.d3304b83.png"
+  },
+  {
+    "revision": "42426e79af439420bcb60505e0d608d3",
+    "url": "/personalsite/static/media/jquery.42426e79.png"
+  },
+  {
+    "revision": "baac61c3bc9c03c042993da6a9dea037",
+    "url": "/personalsite/static/media/me.baac61c3.jpg"
+  },
+  {
+    "revision": "07ed078bff19acc7fa31fefa2f90affa",
+    "url": "/personalsite/static/media/mongodb.07ed078b.png"
+  },
+  {
+    "revision": "4c6c0929cc58f241847a744b2de75e2f",
+    "url": "/personalsite/static/media/node.4c6c0929.png"
+  },
+  {
+    "revision": "e318f8fe18cdde2d12b713dfb8e6fc32",
+    "url": "/personalsite/static/media/python.e318f8fe.png"
+  },
+  {
+    "revision": "2d2b0d56aeefb1b0f3025195cb2ff7b6",
+    "url": "/personalsite/static/media/react.2d2b0d56.png"
+  },
+  {
+    "revision": "cb42bfdb2e9fe261dcb78d5b8a88ac6a",
+    "url": "/personalsite/static/media/resume.cb42bfdb.png"
+  },
+  {
+    "revision": "051d708b4cfbdd0ba6ebe589ba1cde61",
+    "url": "/personalsite/static/media/twitter.051d708b.png"
+  },
+  {
+    "revision": "ce69286c9df18c104e45b4663c835369",
+    "url": "/personalsite/static/media/webtech.ce69286c.png"
+  }
+]);
